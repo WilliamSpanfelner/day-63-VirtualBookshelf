f7e90c511c573e8bd5219c569b6cb1d0384edc09
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-16 21:05:59 +0300

    Create SQLAlchemy db object
    for persistent storage.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///all_books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 all_books = []
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"<Book: {self.title}>"
 
 
 @app.route('/')
